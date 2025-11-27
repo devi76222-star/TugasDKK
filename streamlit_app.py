21aba06357e791d71ca0d1c147dc0d516ec4cece
devi76222-star <inon76222@gmail.com>
2025-11-27 07:39:10 +0700

    Tugas dkk devi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("penjumlahan")
+st.markdown(
+    """ 
+    Devi Apriliana Saputri 9 
+    """
 )
+
+import streamlit as st
+
+st.title("Aplikasi Pertambahan")
+
+# Input angka
+angka1 = st.number_input("Masukkan angka pertama", value=0)
+angka2 = st.number_input("Masukkan angka kedua", value=0)
+
+# Tombol hitung
+if st.button("Hitung"):
+    hasil = angka1 + angka2
+    st.success(f"Hasil pertambahan: {hasil}")
+if st.button("Send balloons!"):
+    st.balloons()
+
